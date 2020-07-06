3cb217d89d90423fd7742ac5f313d59f8dddfaeb
RodrigoAlonsoLobo <67879571+RodrigoAlonsoLobo@users.noreply.github.com>
2020-07-06 13:17:49 -0500

    Add files via upload

--- NewMain.java
+++ NewMain.java
@@ -19,7 +19,7 @@ public class NewMain {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-           int Opci贸n1, Opci贸n2, Opci贸n3;
+           int Opcion, Opcion2, Opcion3;
       int  Cantidad1, Cantidad2, Cantidad3, CostoBruto1, CostoBruto2, CostoBruto3;
        int  CantidadLeche, CantidadLecheLarga, CantidadYogur, CantidadNatilla ;
  int  CantidadLeche1, CantidadLecheLarga1, CantidadYogur1, CantidadNatilla1 ;
@@ -34,22 +34,22 @@ public class NewMain {
  final  double Credito = 1.02 ;
 
    
-ImprimeMen煤1();
-ImprimeOpci贸n1();
+ImprimeMenu1();
+ImprimeOpcion1();
 
-while (Opci贸n != 2){
-switch(Opci贸n1){
+while (Opcion != 2){
+switch(Opcion1){
 	case 1: //Ingresa nuevo cliente
-	ImprimeMen煤2();
-	ImprimeOpci贸n2();
-	while (Opci贸n2 != 3){
-	switch(Opci贸n){
-		case 1:  //compra a cr茅dito
+	ImprimeMenu2();
+	ImprimeOpcion2();
+	while (Opcion2 != 3){
+	switch(Opcion){
+		case 1:  //compra a credito
 		Factura = Factura + 1
-		ImprimeMen煤3();
-		ImprimeOpci贸n3();
-		while (Opci贸n != 5){
-		switch(Opci贸n){
+		ImprimeMenu3();
+		ImprimeOpcion3();
+		while (Opcion != 5){
+		switch(Opcion){
 			case 1: //leche
 	            
 			System.out.println("驴Cuantas cajas de leche desea llevar?");
@@ -67,8 +67,8 @@ switch(Opci贸n1){
 			
 			
 			break;
-			case 2: //leche de larga duraci贸n 
-			System.out.println("驴Cuantas cajas de leche de larga duraci贸n desea llevar?");
+			case 2: //leche de larga duracion 
+			System.out.println("緾uantas cajas de leche de larga duracion desea llevar?");
 			Cantidad1 = Integer.parseInt(entrada.readLine());
 			CostoBruto1 = Leche * Cantidad1
 			CostoBruto2= CostoBruto2 + CostoBruto1
@@ -84,7 +84,7 @@ switch(Opci贸n1){
 			
 			break;
 			case 3: //yogur 
-			System.out.println("驴Cuantas cajas de yogur desea llevar?");
+			System.out.println("緾uantas cajas de yogur desea llevar?");
 			Cantidad1 = Integer.parseInt(entrada.readLine());
 			CostoBruto1 = Leche * Cantidad1
 			CostoBruto2= CostoBruto2 + CostoBruto1
@@ -100,7 +100,7 @@ switch(Opci贸n1){
 			
 			break;
 			case 4: //natilla
-			System.out.println("驴Cuantas cajas de leche desea llevar?");
+			System.out.println("緾uantas cajas de leche desea llevar?");
 			Cantidad1 = Integer.parseInt(entrada.readLine());
 			CostoBruto1 = Leche * Cantidad1
 			CostoBruto2= CostoBruto2 + CostoBruto1
@@ -115,31 +115,31 @@ switch(Opci贸n1){
 			Cantidad2= Cantidad2+Cantidad1
 			break;
 			default:
-			System.out.println("No v谩lido, intente ingresando otro d铆gito");
+			System.out.println("No valido, intente ingresando otro digito");
 			}
 			System.out.println("");
-			ImprimeMen煤3();
-			ImprimeOpci贸n3();
+			ImprimeMenu3();
+			ImprimeOpcion3();
 			}
 			CantidadLeche1 = CantidadLeche1 + CantidadLeche;
 			CantidadLecheLarga1 = CantidadLecheLarga1 + CantidadlecheLarga;
 			CantidadYogur1 = CantidadYogur1 + Cantidadyogur;
 			CantidadNatilla1 = CantidadNatilla1 + CantidadNatilla;
-			System.out.println("Factura N鈥?+Factura+ 鈥淭ipo de venta: Cr茅dito鈥?);
-			System.out.println("Leche鈥?       +CantidadLeche+     +CantidadLeche*Leche);
-			System.out.println("Leche de larga duraci贸n鈥?	+CantidadLecheLarga);
-			System.out.println("Yogur鈥?		+CantidadYogur);
-			System.out.println("Natilla鈥?		+CantidadNatilla);
-			System.out.println("Importe cr茅dito 2%鈥?		+Credito2");
-			System.out.println("IVA 13%鈥?			 +IVA2");
-			System.out.println("TOTAL鈥?			            +CostoNeto2");
+			System.out.println("Factura N" +Factura+ "Tipo de venta: Credito");
+			System.out.println("Leche"       +CantidadLeche+     +CantidadLeche*Leche);
+			System.out.println("Leche de larga duracion"	+CantidadLecheLarga);
+			System.out.println("Yogur"		+CantidadYogur);
+			System.out.println("Natilla"		+CantidadNatilla);
+			System.out.println("Importe credito 2%"		+Credito2");
+			System.out.println("IVA 13%"			 +IVA2");
+			System.out.println("TOTAL"			            +CostoNeto2");
 			}
 		case 2: //compra en efectivo
 		Factura = Factura + 1
-		ImprimeMen煤3();
-		ImprimeOpci贸n3();
-		while (Opci贸n != 5){
-		switch(Opci贸n){
+		ImprimeMenu3();
+		ImprimeOpcion3();
+		while (Opcion != 5){
+		switch(Opcion){
 			case 1: //leche
 	            
 			System.out.println("驴Cuantas cajas de leche desea llevar?");
@@ -155,8 +155,8 @@ switch(Opci贸n1){
 			System.out.println("Leche"		+Cantidad1+ 		+CostoBruto1+);
 			
 			break;
-			case 2: //leche de larga duraci贸n 
-			System.out.println("驴Cuantas cajas de leche de larga duraci贸n desea llevar?");
+			case 2: //leche de larga duracion 
+			System.out.println("緾uantas cajas de leche de larga duraci贸n desea llevar?");
 			Cantidad1 = Integer.parseInt(entrada.readLine());
 			CostoBruto1 = Leche * Cantidad1
 			CostoBruto2= CostoBruto2 + CostoBruto1
@@ -167,10 +167,10 @@ switch(Opci贸n1){
 			Iva2 = IVA2 + IVA1
 			Cantidad2= Cantidad2+Cantidad1
 			Cantidad2= Cantidad2+Cantidad1
-			System.out.println("Leche de larga duraci贸n "  +Cantidad1+  +CostoBruto1+);
+			System.out.println("Leche de larga duracion "  +Cantidad1+  +CostoBruto1+);
 			break;
 			case 3: //yogur 
-			System.out.println("驴Cuantas cajas de yogur desea llevar?");
+			System.out.println("緾uantas cajas de yogur desea llevar?");
 			Cantidad1 = Integer.parseInt(entrada.readLine());
 			CostoBruto1 = Leche * Cantidad1
 			CostoBruto2= CostoBruto2 + CostoBruto1
@@ -184,7 +184,7 @@ switch(Opci贸n1){
 			System.out.println("yogur"		+Cantidad1+ 		+CostoBruto1+);
 			break;
 			case 4: //natilla
-			System.out.println("驴Cuantas cajas de Natilla desea llevar?");
+			System.out.println("緾uantas cajas de Natilla desea llevar?");
 			Cantidad1 = Integer.parseInt(entrada.readLine());
 			CostoBruto1 = Natilla * Cantidad1
 			CostoBruto2= CostoBruto2 + CostoBruto1
@@ -198,31 +198,31 @@ switch(Opci贸n1){
 			System.out.println("Leche"		+Cantidad1+ 		+CostoBruto1+);
 			break;
 			default:
-			System.out.println("No v谩lido, intente ingresando otro d铆gito");
+			System.out.println("No valido, intente ingresando otro d铆gito");
 			}
 			System.out.println("");
-			ImprimeMen煤3();
-			ImprimeOpci贸n3();
+			ImprimeMenu3();
+			ImprimeOpcion3();
 			CantidadLeche1 = CantidadLeche1 + CantidadLeche
 			CantidadLecheLarga1 = CantidadLecheLarga1 + CantidadlecheLarga
 			CantidadYogur1 = CantidadYogur1 + Cantidadyogur
 			CantidadNatilla1 = CantidadNatilla1 + CantidadNatilla
-			System.out.println("Factura N鈥?+Factura+ 鈥淭ipo de venta: Cr茅dito鈥?);
-			System.out.println("Leche鈥?       +CantidadLeche+     +CantidadLeche*Leche);
-			System.out.println("Leche de larga duraci贸n鈥?	+CantidadLecheLarga);
-			System.out.println("Yogur鈥?		+CantidadYogur);
-			System.out.println("Natilla鈥?		+CantidadNatilla);
-			System.out.println("IVA 13%鈥?			 +IVA2");
-			System.out.println("TOTAL鈥?			            +CostoNeto2");
+			System.out.println("Factura" +Factura+ Tipo de venta: Credito?);
+			System.out.println("Leche?       +CantidadLeche+     +CantidadLeche*Leche);
+			System.out.println("Leche de larga duracion?	+CantidadLecheLarga);
+			System.out.println("Yogur"		+CantidadYogur);
+			System.out.println("Natilla"		+CantidadNatilla);
+			System.out.println("IVA 13"			 +IVA2");
+			System.out.println("TOTAL"     +CostoNeto2");
 			}
 
 		break;
 		default:
-		System.out.println("No v谩lido, intente ingresando otro d铆gito");
+		System.out.println("No valido, intente ingresando otro digito");
 		}
 		System.out.println("");
-		ImprimeMen煤2();
-		ImprimeOpci贸n2();
+		ImprimeMenu2();
+		ImprimeOpcion2();
 		}
 			CostoBruto3= CostoBruto3 + CostoBruto2
 			CostoNeto3= CostoNeto3 + CostoNeto2
@@ -233,53 +233,53 @@ switch(Opci贸n1){
 			System.out.println("LecheLarga" +CantidadLecheLarga+   +CantidadLecheLarga*LecheLarga+);
 			System.out.println("Yogur"	+CantidadYogur+ +CantidadYogur*Yogur+);
                                     System.out.println("Natilla"	+CantidadNatilla+ +CantidadNatilla*Yogur+);
-			System.out.println("Importe cr茅dito 2% 鈥?		 +Credito3");
-			System.out.println("TOTAL鈥?		 +CostoNeto3");                                            if (CostoNeto3 <1000001) {  System.out.println("Comisi贸n  " + CostoNeto3*0.03); }  
-if (CostoNeto3 >1000000 {  System.out.println("Comisi贸n  " + CostoNeto3*0.04); } 
+			System.out.println("Importe credito 2%"?		 +Credito3");
+			System.out.println("TOTAL"		 +CostoNeto3");                                            if (CostoNeto3 <1000001) {  System.out.println("Comisi贸n  " + CostoNeto3*0.03); }  
+if (CostoNeto3 >1000000 {  System.out.println("Comision  " + CostoNeto3*0.04); } 
 			}
 
 
 default:
-System.out.println("No v谩lido, intente ingresando otro d铆gito");
+System.out.println("No valido, intente ingresando otro digito");
 }
 
-ImprimeMen煤1();
-ImprimeOpci贸n1();
+ImprimeMenu1();
+ImprimeOpcion();
 }
 System.out.println("Saliendo del programa...");
 System.out.println("Gracias por preferirnos.");
 
 }
-private static void ImprimeMen煤1(){
+private static void ImprimeMenu1(){
 System.out.println("BIENVENIDO");
-System.out.println("Compa帽铆a distribuidora de productos l谩cteos Rica Leche S.A.鈥?); 
+System.out.println("Compania distribuidora de productos lacteos Rica Leche S.A."); 
 System.out.println("1) Ingresar nuevo cliente");
 System.out.println("2) Salir");
 
 }
-private static void ImprimeOpci贸n1() throws IOException{
+private static void ImprimeOpcion1() throws IOException{
 BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
 Opci贸n1 = Integer.parseInt(entrada.readLine());
 
 }
 
 
-private static void ImprimeMen煤2(){
+private static void ImprimeMenu2(){
 System.out.println("Seleccione el tipo de compra que desea鈥?); 
 System.out.println("1) Compra al cr茅dito");
 System.out.println("1) Compra de contado");
 System.out.println("3) Regresar");
 
 }
-private static void ImprimeOpci贸n2() throws IOException{
+private static void ImprimeOpcion2() throws IOException{
 BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
-Opci贸n2 = Integer.parseInt(entrada.readLine());
+Opcion2 = Integer.parseInt(entrada.readLine());
 
 }
 
 
 }
-private static void ImprimeMen煤3(){
+private static void ImprimeMenu3(){
 System.out.println("Ingrese el producto que desea");
 System.out.println("1) Leche");
 System.out.println("2) Leche de larga duraci贸n");
@@ -288,9 +288,9 @@ System.out.println("4) Natilla");
 System.out.println("5) Regresar");
 
 }
-private static void ImprimeOpci贸n3() throws IOException{
+private static void ImprimeOpcion3() throws IOException{
 BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
-Opci贸n3 = Integer.parseInt(entrada.readLine());
+Opcion3 = Integer.parseInt(entrada.readLine());
 
 }
 
